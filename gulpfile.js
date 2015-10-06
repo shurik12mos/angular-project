3da39a047db11ea96317b359b69a519c8fc04f75
*Alex <*aa.yaroshenko@gmail.com*>
2015-10-06 11:36:32 +0300

    second

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,18 @@
 var gulp = require('gulp'),
-	concat = require('gulp-concat');
+	concat = require('gulp-concat'),
+	notify = require('gulp-notify'),
+	watch = require('gulp-watch');
  
 gulp.task('default', function() {
-   gulp.src('./app/*.js')
+   gulp.src('./app/*.js')    
     .pipe(concat('all.js'))
-    .pipe(gulp.dest('./dist/'));
-   gulp.src('./app/*.css')
+    .pipe(gulp.dest('./dist/'))
+	.pipe(notify('Done js!'));
+   gulp.src('./app/*.css')  
 	.pipe(concat('all.css'))
-	.pipe(gulp.dest('./dist/'));
+	.pipe(gulp.dest('./dist/'))
+	.pipe(notify('Done css!'));
+	
+	 watch('app/*.css', function() { gulp.start('default'); });
 });
+
